856b0386af515f5b13e22d22d9e4612a6675814f
rsbrennan-NOAA <reid.brennan@noaa.gov>
2024-12-18 13:48:32 -0800

    dumb mistake

--- scripts/filter.1.sh
+++ scripts/filter.1.sh
@@ -8,7 +8,7 @@
 #SBATCH -n 1
 #SBATCH --mem=16G
 #SBATCH --partition=standard
-#SBATCH --time=4:00:00
+#SBATCH --time=7:00:00
 
 source ~/.bashrc
 
@@ -20,45 +20,37 @@ INDIR=~/Tursiops-NC-PopulationAssignment-RAD/analysis/variants
 echo "starting first missingness filter"
 
 vcftools --gzvcf ${INDIR}/variants_raw_merged.vcf.gz \
-	--max-missing 0.5 --recode-INFO-all --stdout | \
+	--max-missing 0.5 --recode --recode-INFO-all --stdout | \
 	bgzip > ${INDIR}/filtered.1.vcf.gz
 
-NUM_VARIANTS1=$(zcat ${INDIR}/filtered.1.vcf.gz | grep -v '^#' | wc -l)
-echo "Number of variants after missingness filter: ${NUM_VARIANTS1}"
-
-vcfallelicprimitives ${INDIR}/variants_raw_merged.vcf.gz --keep-info --keep-geno | \
-	vcfstreamsort |  bgzip > ${INDIR}/filtered.1.vcf.gz
+echo "done with first missingness filter"
 
 NUM_VARIANTS1=$(zcat ${INDIR}/filtered.1.vcf.gz | grep -v '^#' | wc -l)
-echo "Number of variants after 1st filter: ${NUM_VARIANTS1}"
+echo "Number of variants after first missingness filter: ${NUM_VARIANTS1}"
 
 echo "running vcfallelicprimitives"
 vcfallelicprimitives ${INDIR}/filtered.1.vcf.gz --keep-info --keep-geno | \
 	        vcfstreamsort |  bgzip > ${INDIR}/filtered.2.vcf.gz
 
+echo "done with allelic primitive"
 NUM_VARIANTS2=$(zcat ${INDIR}/filtered.2.vcf.gz | grep -v '^#' | wc -l)
 echo "Number of variants after primitives step: ${NUM_VARIANTS2}"
 
-
-
+echo "start 2nd filtering"
 vcftools --gzvcf ${INDIR}/filtered.2.vcf.gz \
-	        --mac 3 --remove-indels --max-alleles 2 --min-alleles 2 --minQ 30  \
-		--recode-INFO-all --stdout | \
+	        --mac 3 --remove-indels --max-alleles 2 --min-alleles 2 --minQ 30  --max-missing 0.6 \
+		--recode --recode-INFO-all --stdout | \
 		vcftools --vcf - --minDP 3 --recode-INFO-all --stdout | \
 		bgzip > ${INDIR}/filtered.3.vcf.gz
 
+echo "finish second filtering"
+
 NUM_VARIANTS3=$(zcat ${INDIR}/filtered.3.vcf.gz | grep -v '^#' | wc -l)
-echo "Number of variants after quality filters: ${NUM_VARIANTS3}"
+echo "Number of variants after second filters: ${NUM_VARIANTS3}"
 
-# stricter missingness
-vcftools --gzvcf ${INDIR}/filtered.3.vcf.gz --max-missing 0.6 --recode-INFO-all --stdout | \
-	    bgzip > ${INDIR}/filtered.4.vcf.gz
-
-NUM_VARIANTS4=$(zcat ${INDIR}/filtered.4.vcf.gz | grep -v '^#' | wc -l)
-echo "Number of variants after stricter missingness filter: ${NUM_VARIANTS4}"
 
 # check missingness:
 cd ${INDIR}
-vcftools --gzvcf ${INDIR}/filtered.4.vcf.gz --missing-indv
+vcftools --gzvcf ${INDIR}/filtered.3.vcf.gz --missing-indv
 
 
